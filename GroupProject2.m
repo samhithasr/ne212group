96b3695e7cebffa16f4d00a117cfcb5eb4e1d9b0
rohansh11 <123337518+rohansh11@users.noreply.github.com>
2024-11-07 16:16:29 -0500

    Create GroupProject2.m

---
+++ GroupProject2.m
@@ -0,0 +1,24 @@
+% Group members: Sam Somavarapu, Rohan Sharma, Rabiat Giram
+
+% first data set
+randData = rand(1,50);
+
+% second data set
+randnData = randn(1,50);
+
+% plots here
+
+
+% statistical analysis commands
+
+% randData section
+randMean = mean(randData);
+randMedian = median(randData);
+randSTDDEV = std(randData);
+randIQR = iqr(randData);
+
+% randnData section
+randnMean = mean(randnData);
+randnMedian = median(randnData);
+randnSTDDEV = std(randnData);
+randnIQR = iqr(randnData);
